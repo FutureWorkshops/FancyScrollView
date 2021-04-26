9e7188919b86ce1ecccc13257ff9d91ed27455a3
Xavi Moll <xmollv@gmail.com>
2021-04-26 13:52:03 +0200

    Added a background to the back button

--- Sources/FancyScrollView/BackButton.swift
+++ Sources/FancyScrollView/BackButton.swift
@@ -11,13 +11,21 @@ struct BackButton: View {
     var body: some View {
         (presentationMode.wrappedValue.isPresented || hasBeenShownAtLeastOnce) ?
             Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
-               Image(systemName: "chevron.left")
-                   .resizable()
-                   .aspectRatio(contentMode: .fit)
-                   .frame(height: 20, alignment: .leading)
-                   .foregroundColor(color)
-                   .padding(.horizontal, 16)
-                   .font(Font.body.bold())
+                ZStack {
+                    Rectangle()
+                        .frame(width: 44, height: 44)
+                        .foregroundColor(Color(red: 28/255, green: 28/255, blue: 30/255))
+                        .cornerRadius(12)
+                        .padding(.horizontal, 16)
+                        .opacity(0.4)
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 20, alignment: .leading)
+                        .foregroundColor(color)
+                        .padding(.horizontal, 16)
+                        .font(Font.body.bold())
+                }
             }
             .onAppear {
                 self.hasBeenShownAtLeastOnce = true
